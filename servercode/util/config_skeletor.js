50234529e942c2ff6e0590132dcfe92c4cb47f87
Alexio <alexioamota@gmail.com>
2013-12-15 19:55:13 +0000

    Removing my api keys

--- servercode/util/config_skeletor.js
+++ servercode/util/config_skeletor.js
@@ -1,21 +1,41 @@
-_api_key = 'your key';
-_db = 'your db';
+crunchbase_apikey = 'crunch base key';
+_twitter_consumerkey = 'twitter consumer key';
+_twitter_secretkey = 'twitter secret key';
+_nyt_key = 'nyt key';
+_db = 'Startup_Map';
 
-function api_key(){
-	return _api_key; 
+function crunchbase_apikey(){
+	return _crunchbase_apikey;
 }
 
 function db(){
-	return _db;
+        return  'mongodb://127.0.0.1:27017/'+_db;
 }
 
-module.exports = {
-	
-	api_key: function(){
-		return api_key();
-	},
+function twitterKeys(){
 
+	var keys = {
+		consumer: _twitter_consumerkey,
+		secret: _twitter_secretkey
+	};
+
+	return keys;
+}
+
+/*new york times*/
+
+module.exports = {
+	crunch_key: function(){
+		return crunchbase_apikey;
+	},
+		
+	twitter_keys: function(){
+		return twitterKeys();
+	},
+			
 	db: function(){
 		return db();
-	}
+	},
+
+	nytKey:_nyt_key
 }
